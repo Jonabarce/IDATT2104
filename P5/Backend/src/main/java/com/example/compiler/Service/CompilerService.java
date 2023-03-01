7055fb326a6220ce3b40f88b47abf1a9fb71434d
Jonatan Andre Vevang <89488655+Jonabarce@users.noreply.github.com>
2023-03-02 00:34:32 +0100

    Update CompilerService.java

--- P5/Backend/src/main/java/com/example/compiler/Service/CompilerService.java
+++ P5/Backend/src/main/java/com/example/compiler/Service/CompilerService.java
@@ -13,15 +13,20 @@ import java.io.InputStreamReader;
 public class CompilerService {
 
     public String compileCode(Code code) throws IOException {
+        // Hent kildekoden fra Code-objektet
         String sourceCode = code.getStringOfCode();
 
+        // Opprett Docker-kommando for å kjøre Python 3.9-alpine i en beholder
         String[] command = {"docker", "run", "-i", "python:3.9-alpine", "python"};
 
+        // Kjør Docker-prosessen
         Process process = Runtime.getRuntime().exec(command);
 
+        // Skriv kildekoden til standardinngangen til Docker-prosessen
         process.getOutputStream().write(sourceCode.getBytes());
         process.getOutputStream().close();
 
+        // Les eventuelle feilmeldinger fra feilstrømmen til Docker-prosessen
         InputStream errorStream = process.getErrorStream();
         BufferedReader errorReader = new BufferedReader(new InputStreamReader(errorStream));
         StringBuilder errorOutput = new StringBuilder();
@@ -30,6 +35,7 @@ public class CompilerService {
             errorOutput.append(errorLine).append("\n");
         }
 
+        // Les standardutgangen til Docker-prosessen hvis det ikke er noen feil
         StringBuilder output = new StringBuilder();
         if (errorOutput.length() == 0) {
             InputStream inputStream = process.getInputStream();
@@ -39,17 +45,20 @@ public class CompilerService {
                 output.append(line).append("\n");
             }
         } else {
+            // Hvis det er feil, legg til feilmeldingen til output
             output.append(errorOutput);
         }
 
+        // Vent på at Docker-prosessen fullfører
         try {
             process.waitFor();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        // Returner output som en streng
         return output.toString();
     }
 
-}
 
+}
