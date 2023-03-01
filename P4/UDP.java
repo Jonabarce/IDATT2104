f6990a3e40068afdd495379c9a3904c845435238
Jonatan Andre Vevang <89488655+Jonabarce@users.noreply.github.com>
2023-03-01 15:42:55 +0100

    Add files via upload

---
+++ P4/UDP.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+public class UDP {
+    public static void main(String[] args) {
+        Thread serverThread = new Thread(() -> {
+            try {
+                Server.main(null);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+
+        Thread clientThread = new Thread(() ->  {
+            try {
+                Client.main(null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        serverThread.start();
+    }
+
+
+}
